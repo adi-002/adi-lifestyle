968f4808553443c8a984ac9939318680ee97cd3e
aditya <aditsh234@gmail.com>
2021-01-02 23:02:53 +0530

    link further modified

--- src/Mendata.jsx
+++ src/Mendata.jsx
@@ -3,7 +3,7 @@ import tshirt from "../src/images/mentshirt.jpg"
 import pant from "../src/images/menpant.jpg"
 import shoes from "../src/images/menshoes.jpg"
 
-const path = window.location.host
+const path = window.location.host + '/lifestyle'
 console.log(path)
 const Mendata = [
     {
